51b5ddefb2fb6506af9f2025464942fab35c0492
tttruong701 <tttruong701@gmail.com>
2021-01-23 19:05:07 -0800

    Init

---
+++ ui/src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+
+    <div>
+      <head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </head>
+      <h1>Save Our Faves - San Diego</h1>
+    </div>
+  );
+}
+
+export default App;
